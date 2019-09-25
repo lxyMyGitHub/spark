58e296fd8d20a83b91ef2891c160fce68f639d39
liangxiaoyu <lxy_my126@126.com>
2019-09-25 09:55:05 +0800

    pass test bvt

---
+++ study/src/main/java/cn/spark/study/streaming/TestCommitJava.java
@@ -0,0 +1,14 @@
+package cn.spark.study.streaming;
+
+/**
+ * @ClassName TestCommitJava
+ * @Deseription TODO
+ * @Author lxy_m
+ * @Date 2019/9/25 9:54
+ * @Version 1.0
+ */
+public class TestCommitJava {
+    public static void main(String[] args) {
+        System.out.println("'666");
+    }
+}
