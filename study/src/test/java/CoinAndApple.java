ee8aa970af1af1fcbc0013cca98736345ea05365
liangxiaoyu <lxy_my126@126.com>
2019-10-23 17:09:33 +0800

    学习笔记

---
+++ study/src/test/java/CoinAndApple.java
@@ -0,0 +1,11 @@
+package PACKAGE_NAME;
+
+/**
+ * @ClassName CoinAndApple
+ * @Deseription TODO
+ * @Author lxy_m
+ * @Date 2019/10/16 22:21
+ * @Version 1.0
+ */
+public class CoinAndApple {
+}
---
+++ study/src/test/java/OneTest.java
@@ -0,0 +1,11 @@
+package PACKAGE_NAME;
+
+/**
+ * @ClassName OneTest
+ * @Deseription TODO
+ * @Author lxy_m
+ * @Date 2019/10/16 22:18
+ * @Version 1.0
+ */
+public class OneTest {
+}
